a05ae031790b115554368ccb439783adda38e789
brandonhs <brandonnms@gmail.com>
2024-06-10 00:23:14 -0700

    Add timestamp in ns

--- compute/main.py
+++ compute/main.py
@@ -1,10 +1,11 @@
 import lcm
+import time
 from exlcm import quad_command_t
 
 lc = lcm.LCM()
 
 msg = quad_command_t()
-msg.timestamp = 0
+msg.timestamp = time.time_ns()
 msg.position = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 
 lc.publish("EXAMPLE", msg.encode())
