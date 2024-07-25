3d00b3e17f826336995be8f407498bd059aa5547
Easternspork <fumi.okutsu@gmail.com>
2024-07-25 08:25:04 -0700

    Add real BNO055 code

--- compute/BNO055.py
+++ compute/BNO055.py
@@ -1,22 +1,27 @@
 # TODO: Actually implment the reading of the sensor here
 
 import random
+import time
+from Adafruit_BNO055 import BNO055
 
 class BNO055:
 
     def __init__(self):
-        pass
+        self.sensor = BNO055.BNO055(busnum=1)
+        if not self.sensor.begin():
+            raise RuntimeError('Failed to initialize BNO055! Is the sensor connected?')
+
 
     def get_projected_gravity(self):
-        projected_gravity = [random.randint(), random.randint(), random.randint()]
+        projected_gravity = [self.sensor.read_gravity[0], self.sensor.read_gravity[1], self.sensor.read_gravity[2]]
         return projected_gravity
     
     def get_orientation(self):
-        orientation = [random.randint(), random.randint(), random.randint()]
+        orientation = [self.sensor.read_euler[0], self.sensor.read_euler[1], self.sensor.read_euler[2]]
         return orientation
 
     def get_ang_vel(self):
-        ang_vel = [random.randint(), random.randint(), random.randint()]
+        ang_vel = [self.sensor.read_gyroscope[0], self.sensor.read_gyroscope[1], self.sensor.read_gyroscope[2]]
         return ang_vel
     
     
